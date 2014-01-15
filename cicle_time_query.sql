a2949eaee306f7653e9354662258effc6e67a3cc
Aldo Mendez <aldomendez86@gmail.com>
2014-01-14 21:19:16 -0600

    added data to n.json

--- cicle_time_query.sql
+++ cicle_time_query.sql
@@ -1,2 +1,2 @@
 SELECT SERIAL_NUM,PASS_FAIL,To_Char(process_date,'YYYY-MM-DD HH24:MI:SS') process_date,SYSTEM_ID,STEP_NAME,CYCLE_TIME
-FROM lr4_shim_assembly WHERE process_date BETWEEN SYSDATE - 4 AND SYSDATE
+FROM lr4_shim_assembly WHERE process_date > sysdate -1
